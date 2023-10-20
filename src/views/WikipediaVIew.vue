062dd549222f3f1449cb2e853ee3de13b16cd40d
Julie <julie.albini@edu.univ-fcomte.fr>
2023-10-20 15:28:38 +0200

    feat(wikipediaAPI) : code to have information from API

--- src/views/WikipediaVIew.vue
+++ src/views/WikipediaVIew.vue
@@ -1,169 +1,124 @@
-<!-- <template>
-  <RouterLink to="/">Home Page</RouterLink>
-  <div>
-    <h3>Choisissez un mois</h3>
-    <select id="jours"></select>
-
-    <h3>Choisissez un jour</h3>
-    <select id="mois"></select>
-
-    <button @click="recupererDonneeDeSelect">Lancer la recherche</button>
-
-    <div id="events">
-      <h4>Résultats :</h4>
-      <p v-html="wikiData"></p>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import { ref, onMounted } from 'vue'
-
-const wikiData = ref('') // Créez une référence pour stocker les données
+import { onMounted, ref } from 'vue'
+import { client } from '@/utils/axios'
 
 function recupererDonneeDeSelect() {
-  const daySelector = document.getElementById('jours')
-  const monthSelector = document.getElementById('mois')
-  const jour = daySelector.value
-  const mois = monthSelector.value
+  const events = ref([])
+  //jours
+  var daySelector = document.getElementById('jours')
+  var jours = daySelector.value
 
-  getWiki(jour, mois)
+  //mois
+  var monthSelector = document.getElementById('mois')
+  var mois = monthSelector.value
+
+  //annees
+  var anneesSelector = document.getElementById('annees')
+  var annees = parseInt(anneesSelector.value)
+
+  // Faites quelque chose avec la valeur, par exemple, affichez-la dans la console
+  console.log('La valeur sélectionnée est : ' + jours + ' ' + mois + ' ' + annees)
+
+  //fonction
+  events.value = getEvents(jours, mois, annees)
+  console.log('events value =', events.value)
 }
 
-const getWiki = (jour, mois) => {
-  fetch(`https://api.wikimedia.org/core/v1/wikipedia/fr/page/${jour}_${mois}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-      }
-      return response.json()
-    })
-    .then((data) => {
-      // Traitez les données ici et mettez-les à jour dans la référence wikiData
-      console.log(data)
-      wikiData.value = data.source
-    })
-    .catch((error) => {
-      console.error('Fetch Error:', error)
-    })
+async function getEvents(jour, mois, annee) {
+  try {
+    const response = await client.get(`/search/${jour}/${mois}/${annee}`)
+    const data = response.data
+
+    // Assuming you want to display all descriptions in the events div
+    const eventsDiv = document.getElementById('events')
+    eventsDiv.innerHTML = '' // Clear existing content
+
+    for (let i = 0; i < data.length; i++) {
+      const description = data[i].description
+
+      // Create a new paragraph element for each description
+      const paragraph = document.createElement('p')
+      paragraph.textContent = description
+
+      // Append the paragraph to the events div
+      eventsDiv.appendChild(paragraph)
+    }
+  } catch (error) {
+    // Handle errors
+    console.error(error)
+  }
 }
 
-onMounted(() => {
+onMounted(async () => {
   const daySelector = document.getElementById('jours')
   const monthSelector = document.getElementById('mois')
+  const yearSelector = document.getElementById('annees')
 
+  // Remplir les sélecteurs avec des options pour les jours, les mois et les années
   for (let i = 1; i <= 31; i++) {
     const option = document.createElement('option')
-    option.value = i
-    option.text = i
+    if (i < 10) {
+      option.value = '0' + i
+      option.text = '0' + i
+    } else {
+      option.value = i
+      option.text = i
+    }
+
     daySelector.appendChild(option)
   }
 
   const months = [
-    'janvier',
-    'février',
-    'mars',
-    'avril',
-    'mai',
-    'juin',
-    'juillet',
-    'août',
-    'septembre',
-    'octobre',
-    'novembre',
-    'décembre'
+    'Janvier',
+    'Fevrier',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Decembre'
   ]
   for (let i = 0; i < months.length; i++) {
     const option = document.createElement('option')
-    option.value = months[i].toLowerCase()
     option.text = months[i]
+    option.value = months[i]
     monthSelector.appendChild(option)
+  }
+
+  const currentYear = new Date().getFullYear()
+  for (let i = currentYear; i >= currentYear - 100; i--) {
+    const option = document.createElement('option')
+    option.value = i
+    option.text = i
+    yearSelector.appendChild(option)
   }
 })
 </script>
+
+<template>
+  <div>
+    <h3>Choississez un mois</h3>
+    <select id="jours"></select>
+
+    <h3>Choississez un jour</h3>
+    <select id="mois"></select>
+
+    <h3>Choississez une année</h3>
+    <select id="annees"></select>
+
+    <button @click="recupererDonneeDeSelect()">Lancer la recherche</button>
+
+    <div id="events"></div>
+  </div>
+</template>
 
 <style>
 #map {
   width: 100%;
   height: 70vh;
 }
-</style> -->
-
-<template>
-  <div>
-    <h1>Section "Sciences et techniques" du 2 février sur Wikipédia</h1>
-    <div v-if="sectionData">
-      <h2>{{ sectionData.title }}</h2>
-      <div v-html="sectionData.content"></div>
-    </div>
-  </div>
-
-  <br />
-
-  <div>
-    <h1>Section "Sciences et techniques" du 2 février sur Wikipédia</h1>
-    <div v-if="ee">
-      <h2>{{ ee.title }}</h2>
-      <div v-html="ee.content"></div>
-    </div>
-  </div>
-</template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      sectionData: null,
-      ee: null
-    }
-  },
-  mounted() {
-    // Appel à l'API Wikipédia pour récupérer la section "Sciences et techniques" du 2 février
-    axios
-      .get('https://fr.wikipedia.org/w/api.php', {
-        params: {
-          action: 'parse',
-          page: '2_février',
-          section: '13', // L'ID de la section "Sciences et techniques" peut varier
-          format: 'json',
-          origin: '*'
-        }
-      })
-      .then((response) => {
-        if (response.data && response.data.parse) {
-          this.sectionData = {
-            title: response.data.parse.title,
-            content: response.data.parse.text['*']
-          }
-        }
-      })
-      .catch((error) => {
-        console.error('Erreur lors de la récupération des données :', error)
-      })
-
-    axios
-      .get('https://fr.wikipedia.org/w/api.php', {
-        params: {
-          action: 'parse',
-          page: '3_février',
-          section: '13', // L'ID de la section "Sciences et techniques" peut varier
-          format: 'json',
-          origin: '*'
-        }
-      })
-      .then((response) => {
-        if (response.data && response.data.parse) {
-          this.ee = {
-            title: response.data.parse.title,
-            content: response.data.parse.text['*']
-          }
-        }
-      })
-      .catch((error) => {
-        console.error('Erreur lors de la récupération des données :', error)
-      })
-  }
-}
-</script>
+</style>
